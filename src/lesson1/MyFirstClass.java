9220e66db2749e01d12846e521cabd1d744f2c50
olgazaigraewa <ol.zaigraewa@yandex.ru>
2022-10-31 20:12:02 +0300

    ДЗ

--- src/lesson1/MyFirstClass.java
+++ src/lesson1/MyFirstClass.java
@@ -4,7 +4,7 @@ package lesson1;
 public class MyFirstClass {
 
     public static void main(String[] args) {
-        Book book = new Book("Dreamers", "Nosov", "Nosov", 1997);
+        Book book = new Book("Dreamers", "Nikolai", "Nosov", 1997);
         System.out.println("book.name = " + book.getName());
         Author author = new Author("Nikolai", "Nosov");
         System.out.println("author.firstName = " + author.getFirstName());
@@ -17,7 +17,7 @@ public class MyFirstClass {
 
         System.out.println(" ");
 
-        Book book1 = new Book("MasterAndMargarita", "Bulgakov", "Bulgakov", 1990);
+        Book book1 = new Book("MasterAndMargarita", " Mikhail", "Bulgakov", 1990);
         System.out.println("book1.getName() = " + book1.getName());
         Author author1 = new Author(" Mikhail", "Bulgakov");
         System.out.println("author1.getFirstName() = " + author1.getFirstName());
